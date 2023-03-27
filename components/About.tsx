92c13e8dfc1b091ca9c606ff2356ab5585537431
manlikexavy <chibokaxavier@gmail.com>
2023-03-27 08:52:22 -0700

    .

--- components/About.tsx
+++ components/About.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 const About = (props: Props) => {
   return (
-    <div className="h-screen flex flex-col relative text-center md:text-left  md:flex-row max-w-7xl justify-evenly items-center mx-auto px-10">
+    <div className="h-screen flex flex-col relative text-center md:text-left  md:flex-row max-w-7xl md:justify-evenly items-center mx-auto px-8">
       <h2 className="absolute top-20 uppercase tracking-[20px] text-2xl text-gray-500 md:pl-0 pl-4">
         About
       </h2>
@@ -24,15 +24,15 @@ const About = (props: Props) => {
           duration: 1.2,
         }}
         src="./images/zhye.jpg"
-        className=" md:mb-0 mt-[60px] md:mt-0   flex-shrink-0 w-[170px] h-[170px] rounded-full object-cover md:rounded-lg md:h-96 md:w-64 xl:w-[400px] xl:h-[450px]"
+        className="  md:mb-0 mt-[140px] md:mt-0   flex-shrink-0 w-[170px] h-[170px] rounded-full object-cover md:rounded-lg md:h-96 md:w-64 xl:w-[400px] xl:h-[450px]"
       />
-      <div className=" px-0 md:px-10 md:pt-0">
+      <div className=" px-0 md:px-10 md:pt-0 ">
         {/* <h4 className=" text-2xl md:text-3xl font-semibold">
           Here is a{" "}
           <span className="underline decoration-[orange]">little</span>{" "}
           Background
         </h4> */}
-        <p className="text-base font-semibold md:text-lg">
+        <p className="text-base font-semibold md:text-lg mt-6">
           {" "}
           Grace Uche-Odii Ezinne is a freelance copy/content writer with
           experience in helping cryptocurrency/blockchain and web3 brands create
